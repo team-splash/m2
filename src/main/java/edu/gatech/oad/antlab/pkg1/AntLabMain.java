170409e2b0dbb8af0e6fa3d7d6e206e7f03b3752
matthew <m5w@users.noreply.github.com>
2018-09-05 21:53:30 -0400

    Add last name

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,7 +65,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("gburdell2");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("Matthew");
+		  Person3 p3 = new Person3("Matthew Marting");
 		  toPrint += p3.toString("mmarting3");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
