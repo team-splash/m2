634aa40da62c5cb95f229e21bc9c9ceaff408d4b
Christopher Obando <chrisjobando@gmail.com>
2018-09-10 09:54:16 -0400

    Edited GTID for person1 in AntLabMain

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -58,7 +58,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
 		  Person1 p1 = new Person1("Chris");
-		  toPrint += p1.toString("gburdell1");
+		  toPrint += p1.toString("cobando3");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("P2");
